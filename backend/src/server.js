82c8ce188c03309297e03117a3ae8ea534b33ffe
nadeeshikagimhani <nadeeshikagimhani4@gmail.com>
2026-04-13 09:23:37 +0200

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,23 +5,42 @@ import notesRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 
+import path from 'path';
+
+import cors from 'cors';
+
 //acces for .env file
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve()
 
 //1
 const app = express();
 
 // middleware
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin: "http://localhost:5173",
+      }
+    ));
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
-
 //use for routes
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.use((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 //connect db
 connectDB().then(() => {
